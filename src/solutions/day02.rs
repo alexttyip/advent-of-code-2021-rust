7bb358a36a18bdf157fa3df14f69b24371765c53
Alex Yip <tsztoyip@gmail.com>
2022-11-27 16:03:23 +0000

    Day 2 done

--- src/solutions/day02.rs
+++ src/solutions/day02.rs
@@ -1,23 +1,58 @@
 use itertools::Itertools;
 use std::fs;
 
-type InputType = Vec<String>;
+type InputType = Vec<(String, i32)>;
 
 fn read_input() -> InputType {
     fs::read_to_string("./inputs/day02.txt")
         .unwrap()
         .trim()
         .lines()
-        .map_into()
+        .map(|line| line.split(' '))
+        // .map(|a| a.collect::<Vec<&str>>())
+        .map(|mut a| {
+            (
+                a.next().unwrap().to_string(),
+                a.next().map(|b| b.parse().unwrap()).unwrap(),
+            )
+        })
         .collect()
 }
 
-fn part1(_input: InputType) -> i64 {
-    0
+fn part1(input: InputType) -> i32 {
+    let mut horizontal = 0;
+    let mut depth = 0;
+
+    for (op, mag) in input {
+        match op.as_str() {
+            "forward" => horizontal += mag,
+            "down" => depth += mag,
+            "up" => depth -= mag,
+            _ => (),
+        }
+    }
+
+    horizontal * depth
 }
 
-fn part2(_input: InputType) -> i64 {
-    0
+fn part2(input: InputType) -> i32 {
+    let mut horizontal = 0;
+    let mut depth = 0;
+    let mut aim = 0;
+
+    for (op, mag) in input {
+        match op.as_str() {
+            "forward" => {
+                horizontal += mag;
+                depth += aim * mag;
+            }
+            "down" => aim += mag,
+            "up" => aim -= mag,
+            _ => (),
+        }
+    }
+
+    horizontal * depth
 }
 
 pub fn run() {
