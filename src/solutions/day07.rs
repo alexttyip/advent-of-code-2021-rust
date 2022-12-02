80d6e5260f0d865853241570ab7e1694c5c0d92a
Alex Yip <tsztoyip@gmail.com>
2022-12-02 00:47:37 +0000

    Day 7 done

--- src/solutions/day07.rs
+++ src/solutions/day07.rs
@@ -1,23 +1,44 @@
-use itertools::Itertools;
 use std::fs;
 
-type InputType = Vec<String>;
+type InputType = Vec<u32>;
 
 fn read_input() -> InputType {
     fs::read_to_string("./inputs/day07.txt")
         .unwrap()
         .trim()
-        .lines()
-        .map_into()
+        .split(',')
+        .map(|c| c.parse().unwrap())
         .collect()
 }
 
-fn part1(_input: InputType) -> i64 {
-    0
+fn part1(input: InputType) -> u32 {
+    let mut sorted = input;
+    sorted.sort_unstable();
+
+    let median = sorted.get(sorted.len() / 2).unwrap();
+
+    sorted.iter().map(|n| n.abs_diff(*median)).sum()
 }
 
-fn part2(_input: InputType) -> i64 {
-    0
+fn part2(input: InputType) -> u32 {
+    let min: u32 = *input.iter().min().unwrap();
+    let max: u32 = *input.iter().max().unwrap();
+
+    let mut min_diff = u32::MAX;
+
+    for i in min..=max {
+        let diff = input
+            .iter()
+            .map(|n| i.abs_diff(*n))
+            .map(|dist| dist * (dist + 1) / 2)
+            .sum();
+
+        if diff < min_diff {
+            min_diff = diff;
+        }
+    }
+
+    min_diff
 }
 
 pub fn run() {
